dfeea627e8b64be1cec3164b040454a3fd876109
Nahum <losond8@gmail.com>
2021-02-07 15:42:26 -0800

    JavaScript Calculator Challenge Lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract (a, b) {
+    let c = a - b;
+    return c
+}
+
+let multiply = (a, b) => {
+    return a * b
+}
+
+let divide = (a, b) => {
+    return a / b
+}
+
+let increment = (n) => {
+    // n++
+    return ++n
+}
+
+let decrement = (n) => {
+    n--
+    return n
+}
+
+function makeInt(n){
+     return parseInt(n, 10)
+
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
